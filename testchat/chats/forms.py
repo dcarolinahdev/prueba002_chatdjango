379a0d1f4e54a56b2aa01d83a21c7695bad5bb0f
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2020-12-02 18:23:58 -0500

    Se cambia el formulario django de mensajes a ModelForm

--- testchat/chats/forms.py
+++ testchat/chats/forms.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 """Chat forms."""
 
+# Django
 from django import forms
 
-class MessageForm(forms.Form):
-	image_msg = forms.ImageField()
+# Model
+from chats.models import Message
+
+class MessageForm(forms.ModelForm):
+	"""Message model form."""
+
+	class Meta:
+		"""Form settings."""
+		model = Message
+		fields = ('user', 'profile', 'image_msg')
